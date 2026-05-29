5e623101368f12425e5e92edb24f632fb4e89fd5
sebrinamusbah <sebrinm9@gmail.com>
2026-05-29 13:42:34 +0300

    Create DashboardUpdated.php

---
+++ app/Events/DashboardUpdated.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DashboardUpdated implements ShouldBroadcast
+{
+    use Dispatchable, SerializesModels;
+
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('dashboard'),
+        ];
+    }
+}
