f2ea3c8b582b774291ff7ade9de5e70a28fff05a
sebrinamusbah <sebrinm9@gmail.com>
2026-07-05 18:59:15 +0300

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,17 +9,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-               $table->timestamp('email_verified_at')->nullable(); 
-            $table->string('password');
-$table->string('status')->default('active');
-            $table->timestamp('last_activity')->nullable();
-            $table->boolean('must_reset_password')->default(true);
-            $table->rememberToken(); // creates remember_token VARCHAR(100) NULL
-            $table->timestamps();
-        });
+    $table->id();
+    $table->string('name');
+
+    $table->string('email')->unique();
+
+    $table->timestamp('email_verified_at')->nullable();
+    $table->string('password');
+
+    $table->enum('status', ['active', 'inactive'])->default('active');
+    $table->timestamp('last_activity')->nullable();
+    $table->boolean('must_reset_password')->default(true);
+
+    $table->rememberToken();
+    $table->timestamps();
+});
     }
 
     public function down(): void
